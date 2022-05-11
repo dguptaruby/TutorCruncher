908d4c81d19f279aff293bfa7b154a5abc60d467
Dipak Gupta <dade.dipakg@gmail.com>
2022-05-11 16:35:14 +0530

    create view and url mapping

--- DevTest/urls.py
+++ DevTest/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from django.views.generic import TemplateView
+from WorldBank.views import metricData
 
 from django.contrib import admin
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html')),
+    # path('', TemplateView.as_view(template_name='index.html')),
+    path('', metricData),  # get all metric data
+    path('<int:metric_id>/', metricData),  # filter by metric id
+    path('<str:country_code>/', metricData),  # filter by country code
+    path('<int:metric_id>/<str:country_code>/', metricData),  # filter by country code and metric id
     path('admin/', admin.site.urls),
 ]
--- WorldBank/views.py
+++ WorldBank/views.py
@@ -1,1 +1,34 @@
 from django.shortcuts import render
+
+from .models import Value
+
+
+def metricData(request):
+    # query param
+    metric_id = request.GET.get("metric_id")
+    country_code = request.GET.get("country_code")
+    order_type = request.GET.get("order_type")
+
+    if not metric_id and not country_code:
+        # get all metric data
+        metric_data = Value.objects.all()
+        if order_type == 'desc':
+            metric_data = metric_data.order_by("-country_code")
+    elif metric_id and not country_code:
+        # only metric id filter data
+        metric_data = Value.objects.filter(metric_id=metric_id)
+        if order_type == 'desc':
+            metric_data = metric_data.order_by("-country_code")
+    elif country_code and not metric_id:
+        # only country code filter data
+        metric_data = Value.objects.filter(country_code=country_code)
+        if order_type == 'desc':
+            metric_data = metric_data.order_by("-country_code")
+    else:
+        # filter country and filter data
+        metric_data = Value.objects.filter(country_code=country_code, metric_id=metric_id)
+        if order_type == 'desc':
+            metric_data = metric_data.order_by("-country_code")
+
+    context = {'metric_obj': metric_data}
+    return render(request, 'metric_list.html', context)
