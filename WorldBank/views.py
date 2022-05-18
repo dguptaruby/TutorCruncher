72c7d48bb7584b64aaef747b15987b5134c3614a
Dipak Gupta <dade.dipakg@gmail.com>
2022-05-18 15:33:12 +0530

    change design and views for metric aggregation data

--- WorldBank/views.py
+++ WorldBank/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
-
+from django.db.models import Avg, Count, Min, Sum, Max
 from .models import Value
+
+
+def getAggregationData(filter_key):
+    # get aggregate value from years column
+    filter_key1 = filter_key + '__isnull'
+    my_filter = {filter_key1: True}
+
+    valueObj = Value.objects.exclude(**my_filter)
+    valueObj = valueObj.aggregate(record_count=Count(filter_key), record_max=Max(filter_key),
+                                  record_min=Min(filter_key), record_avg=Avg(filter_key),
+                                  record_sum=Sum(filter_key))
+    return valueObj
 
 
 def metricData(request):
@@ -8,6 +20,7 @@ def metricData(request):
     metric_id = request.GET.get("metric_id")
     country_code = request.GET.get("country_code")
     order_type = request.GET.get("order_type")
+    record_years = request.GET.get("record_year")
 
     if not metric_id and not country_code:
         # get all metric data
@@ -30,5 +43,9 @@ def metricData(request):
         if order_type == 'desc':
             metric_data = metric_data.order_by("-country_code")
 
-    context = {'metric_obj': metric_data}
+    aggregate_values = None
+    rec_val = record_years if record_years else "2004"
+    aggregate_values = getAggregationData("record_" + rec_val)
+
+    context = {'metric_obj': metric_data, "aggregate_obj": aggregate_values}
     return render(request, 'metric_list.html', context)
